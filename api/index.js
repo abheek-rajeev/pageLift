5b1f71f5b7e956fd2f4770ddbe6cd88b4fb3ceac
abheek-rajeev <abheekrajeev@gmail.com>
2024-03-27 03:39:16 +0530

    creating user model

--- api/index.js
+++ api/index.js
@@ -5,8 +5,11 @@ dotenv.config();
 
 const app=express();
 
-mongoose.connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to database")
+})
 
 app.listen(3000,()=>{
     console.log("server running at port 3000");
 });
+
